9e607b2539d38928fd2782ce3673f5705368a405
lophiester <gilcharles2022@hotmail.com>
2024-01-15 15:10:34 +0900

    Initial Commit

---
+++ ChatGPTAPP/ChatGPTAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGPTAPPApp.swift
+//  ChatGPTAPP
+//
+//  Created by Charles Yamamoto on 2024/01/15.
+//
+
+import SwiftUI
+
+@main
+struct ChatGPTAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
